60607a7adfb67a65928f571ea5cff40584e01ea4
lqingfang <lqingfang_279@163.com>
2019-03-04 21:33:41 +0800

    enum 使用

---
+++ testLearn/src/main/java/org/lqf/learn/enumm/FruitEnum.java
@@ -0,0 +1,7 @@
+package org.lqf.learn.enumm;
+
+public enum FruitEnum {
+    Apple,
+    Orange,
+    None
+}
---
+++ testLearn/src/main/java/org/lqf/learn/enumm/Test.java
@@ -0,0 +1,10 @@
+package org.lqf.learn.enumm;
+
+public class Test {
+    private static FruitEnum fruitEnum = FruitEnum.Apple;
+
+    public static void main(String[] args) {
+
+        System.out.println(fruitEnum.toString());
+    }
+}
