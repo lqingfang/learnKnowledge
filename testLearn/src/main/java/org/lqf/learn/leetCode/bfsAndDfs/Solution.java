a6cc09af977f6535bb4960104d277d45702469d8
lqingfang <lqingfang_279@163.com>
2019-11-14 22:12:14 +0800

    785图不太好理解，理解了7分熟的样仔

---
+++ testLearn/src/main/java/org/lqf/learn/leetCode/bfsAndDfs/Solution.java
@@ -0,0 +1,34 @@
+package org.lqf.learn.leetCode.bfsAndDfs;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[][] graph = new int[][]{
+             {1,3}, {0,2}, {1,3}, {0,2}
+        };
+        System.out.println(isBipartite(graph));
+    }
+    public static boolean isBipartite(int[][] graph) {
+        if(null == graph || graph.length == 0) {
+            return false;
+        }
+        int[] colors = new int[graph.length];
+        for(int i=0;i<graph.length;i++) {
+            if(!dfs(graph,i,colors,0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private static boolean dfs(int[][] graph, int i, int[] colors, int lastColor) {
+        if(colors[i] != 0) {
+            return colors[i] != lastColor;
+        }
+        colors[i] = lastColor ==1?2:1;
+        for(int j=0;j<graph[i].length;j++) {
+            if(!dfs(graph,graph[i][j],colors,colors[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
