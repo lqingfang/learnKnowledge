ca2aff0a0b4463ae33c3678a17ed7142a7dd7a6c
lqingfang <lqingfang_279@163.com>
2019-12-21 17:24:31 +0800

    78典型的回溯算法

---
+++ testLearn/src/main/java/org/lqf/learn/leetCode/backTrack/subsets_78/Solution.java
@@ -0,0 +1,28 @@
+package org.lqf.learn.leetCode.backTrack.subsets_78;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(subsets(nums));
+    }
+    static List<List<Integer>> res = new ArrayList<>();
+    public static List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        dfs(nums,0,new Stack<Integer>());
+        return res;
+    }
+
+    private static void dfs(int[] nums,int start,Stack stack) {
+        res.add(new ArrayList<>(stack));
+        for(int i=start;i<nums.length;i++) {
+            stack.add(nums[i]);
+            dfs(nums,i+1,stack);
+            stack.pop();
+        }
+    }
+}
