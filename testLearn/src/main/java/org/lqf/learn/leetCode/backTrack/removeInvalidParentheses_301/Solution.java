c17a3349e8a35e61dcc05e3db4d6d0659a6df5d7
lqingfang <lqingfang_279@163.com>
2019-12-21 11:32:06 +0800

    301删除无效括号，理解3，4成吧

---
+++ testLearn/src/main/java/org/lqf/learn/leetCode/backTrack/removeInvalidParentheses_301/Solution.java
@@ -0,0 +1,64 @@
+package org.lqf.learn.leetCode.backTrack.removeInvalidParentheses_301;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s= "3(()())(()";
+        System.out.println(removeInvalidParentheses(s));
+    }
+
+    static Set<String> ans = new HashSet<>();
+    public static List<String> removeInvalidParentheses(String s) {
+        List<String> res = new ArrayList<>();
+        int leftDel = 0;
+        int rightDel = 0;
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i) == '(') {
+                leftDel++;
+            }else if(s.charAt(i) == ')') {
+                if(leftDel > 0){
+                    leftDel--;
+                }else {
+                    rightDel++;
+                }
+            }
+        }
+        dfs(s,0,0,leftDel,rightDel,new StringBuilder());
+        res.addAll(ans);
+        return res;
+    }
+    private static void dfs(String s, int index, int leftCount, int leftDel, int rightDel, StringBuilder sb) {
+        if(index == s.length()) {
+            if(leftDel == 0 && rightDel == 0) {
+                ans.add(sb.toString());
+            }
+            return;
+        }
+        char c = s.charAt(index);
+        if( c == '(') {
+            if(leftDel > 0) {
+                dfs(s,index+1,leftCount,leftDel-1,rightDel,sb);
+            }
+            sb.append(c);
+            dfs(s,index+1,leftCount+1,leftDel,rightDel,sb);
+            sb.deleteCharAt(sb.length()-1);
+        }else if(c == ')') {
+            if(rightDel > 0) {
+                dfs(s,index+1,leftCount,leftDel,rightDel-1,sb);
+            }
+            if(leftCount > 0) {
+                sb.append(c);
+                dfs(s,index+1,leftCount-1,leftDel,rightDel,sb);
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }else {
+            sb.append(c);
+            dfs(s,index+1,leftCount,leftDel,rightDel,sb);
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
+}
