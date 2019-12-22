b63fc234d7c3966db9857b0e0c078afc31750969
lqingfang <lqingfang_279@163.com>
2019-12-22 17:55:30 +0800

    96 动态规划，嗯，这是一道数学题

---
+++ testLearn/src/main/java/org/lqf/learn/leetCode/backTrack/numTrees_96/Solution.java
@@ -0,0 +1,26 @@
+package org.lqf.learn.leetCode.backTrack.numTrees_96;
+
+/**
+ * 分治
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(numTrees(3));
+    }
+    public static int numTrees(int n) {
+        return getTrees(1,n);
+    }
+
+    private static int getTrees(int start, int end) {
+        int ans =0;
+        if(start >=end) {
+            return 1;
+        }
+        for(int i=start;i<= end;i++) {
+            int left = getTrees(start,i-1);
+            int right = getTrees(i+1,end);
+            ans += left*right;
+        }
+        return ans;
+    }
+}
