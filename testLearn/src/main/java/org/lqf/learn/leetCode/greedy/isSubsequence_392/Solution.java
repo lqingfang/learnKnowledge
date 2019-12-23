242928b7f1ea1375af6537a42e72e742ea27df61
lqingfang <lqingfang_279@163.com>
2019-12-23 22:56:36 +0800

    392这道题之前做过，或者做过类似的，嘿嘿，有点信心，至少简单的能够做出来，不过这个确实简单

---
+++ testLearn/src/main/java/org/lqf/learn/leetCode/greedy/isSubsequence_392/Solution.java
@@ -0,0 +1,24 @@
+package org.lqf.learn.leetCode.greedy.isSubsequence_392;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbgdc";
+        System.out.println(isSubsequence(s,t));
+    }
+    public static boolean isSubsequence(String s, String t) {
+        if(null ==s ||s.length() ==0) {
+            return true;
+        }
+        int index =0;
+        for(int i=0;i<t.length();i++) {
+            if(t.charAt(i) == s.charAt(index)) {
+                index++;
+            }
+            if(index == s.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
