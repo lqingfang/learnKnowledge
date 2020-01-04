1ef0017b762ce78b9fbd7fbdc0e0f7f5bdb81d23
lqingfang <lqingfang_279@163.com>
2020-01-04 20:42:47 +0800

    693题意没懂

---
+++ testLearn/src/main/java/org/lqf/learn/leetCode/bitwise/hasAlternatingBits_693/Solution.java
@@ -0,0 +1,10 @@
+package org.lqf.learn.leetCode.bitwise.hasAlternatingBits_693;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public boolean hasAlternatingBits(int n) {
+        return false;
+    }
+}
