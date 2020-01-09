5a7cc6580bece51983ba84a3bce2e6539d228130
lqingfang <lqingfang_279@163.com>
2020-01-09 23:08:01 +0800

    1

--- testLearn/src/main/java/org/lqf/learn/leetCode/dynamic/canPartition_416/Solution.java
+++ testLearn/src/main/java/org/lqf/learn/leetCode/dynamic/canPartition_416/Solution.java
@@ -3,7 +3,7 @@ package org.lqf.learn.leetCode.dynamic.canPartition_416;
 public class Solution {
     public static void main(String[] args) {
         //int[] nums = {1,3,4,4};
-        int[] nums = {1,2,5};
+        int[] nums = {1,2,5,4,6};
         System.out.println(canPartition(nums));
     }
     public static boolean canPartition(int[] nums) {
@@ -13,7 +13,6 @@ public class Solution {
         if(nums.length == 2) {
             return nums[0]==nums[1];
         }
-        int len = nums.length;
         int sum = 0;
         for(int num:nums) {
             sum += num;
