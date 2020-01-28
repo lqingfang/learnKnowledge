bbbdd79dd2a3551f90ae8237fa0660667809300f
lqingfang <lqingfang_279@163.com>
2020-01-28 20:38:03 +0800

    1

--- testLearn/src/main/java/org/lqf/learn/leetCode/dynamic/wordBreak_139/Solution.java
+++ testLearn/src/main/java/org/lqf/learn/leetCode/dynamic/wordBreak_139/Solution.java
@@ -12,16 +12,16 @@ public class Solution {
     public static boolean wordBreak(String s, List<String> wordDict) {
         int len = s.length();
         //memo[i]表示s中以i-1结尾的字符串是否再wordDict中，两层循环，一点一点逼近，最后memo[len]就是答案
-        boolean[] memo = new boolean[len+1];
-        memo[0] = true;
+        boolean[] dp = new boolean[len+1];
+        dp[0] = true;
         for(int i=1;i<=len;i++) {
             for(int j=0;j<i;j++) {
-                if(memo[j] && wordDict.contains(s.substring(j,i))) {
-                    memo[i] = true;
+                if(dp[j] && wordDict.contains(s.substring(j,i))) {
+                    dp[i] = true;
                     break;
                 }
             }
         }
-        return memo[len];
+        return dp[len];
     }
 }
