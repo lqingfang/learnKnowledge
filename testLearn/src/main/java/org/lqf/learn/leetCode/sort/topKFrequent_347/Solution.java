2e88bf9504ee351886be0ff57880d3096f2a6f35
lqingfang <lqingfang_279@163.com>
2020-03-15 20:16:09 +0800

    347 看答案了，使用到了堆，排序

---
+++ testLearn/src/main/java/org/lqf/learn/leetCode/sort/topKFrequent_347/Solution.java
@@ -0,0 +1,31 @@
+package org.lqf.learn.leetCode.sort.topKFrequent_347;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3};
+        System.out.println(topKFrequent(nums,2));
+    }
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> countMap = new HashMap<>();
+        for(int num:nums) {
+            countMap.put(num,countMap.getOrDefault(num,0)+1);
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>((o1,o2)->
+            countMap.get(o1) - countMap.get(o2)
+        );
+        for(int n:countMap.keySet()) {
+            queue.add(n);
+            if(queue.size()>k) {
+                queue.poll();
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            res.add(queue.poll());
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
