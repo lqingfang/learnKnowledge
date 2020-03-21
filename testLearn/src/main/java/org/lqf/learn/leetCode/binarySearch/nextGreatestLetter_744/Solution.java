e35ef63a5413065637ff8c1d25b1274327e77747
lqingfang <lqingfang_279@163.com>
2020-03-22 00:40:08 +0800

    744 二分二分，这种的要想二分，就像之前想 hash一样。。

---
+++ testLearn/src/main/java/org/lqf/learn/leetCode/binarySearch/nextGreatestLetter_744/Solution.java
@@ -0,0 +1,28 @@
+package org.lqf.learn.leetCode.binarySearch.nextGreatestLetter_744;
+
+public class Solution {
+    public static void main(String[] args) {
+        char[] letters = {'a','c','d'};
+        char target = 'c';
+        System.out.println(nextGreatestLetter(letters,target));
+    }
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length-1;
+        if(letters.length <1) {
+            return ' ';
+        }
+        if(letters[letters.length-1] <= target) {
+            return letters[0];
+        }
+        while(left <= right) {
+            int mid = (left+right) >>1;
+            if(letters[mid] > target) {
+                right = mid-1;
+            }else {
+                left = mid+1;
+            }
+        }
+        return letters[left];
+    }
+}
