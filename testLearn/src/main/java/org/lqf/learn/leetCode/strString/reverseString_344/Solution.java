e5aef188a1cae6cbd1d28d3ae47eab1c45e3f686
lqingfang <lqingfang_279@163.com>
2020-04-01 01:28:38 +0800

    344 自己写的哈

---
+++ testLearn/src/main/java/org/lqf/learn/leetCode/strString/reverseString_344/Solution.java
@@ -0,0 +1,15 @@
+package org.lqf.learn.leetCode.strString.reverseString_344;
+
+public class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+        while (left<right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right]= temp;
+            left++;
+            right--;
+        }
+    }
+}
