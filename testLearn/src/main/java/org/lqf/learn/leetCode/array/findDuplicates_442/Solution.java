d545bae88a2da0caa8729e21f48134ee38e6a728
lqingfang <lqingfang_279@163.com>
2020-04-05 19:28:37 +0800

    442 自己写的，一次通过啊

---
+++ testLearn/src/main/java/org/lqf/learn/leetCode/array/findDuplicates_442/Solution.java
@@ -0,0 +1,18 @@
+package org.lqf.learn.leetCode.array.findDuplicates_442;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++) {
+            if(nums[i] == nums[i-1]) {
+                res.add(nums[i]);
+            }
+        }
+        return res;
+    }
+}
